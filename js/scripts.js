8c42573125ac192863b0c4d77fc690270efc2a99
Joisann <joisann777@gmail.com>
2017-11-13 21:00:58 +0100

    adding files

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+var a = 2;
+	b = 3;
+	value = (a * a) - (2 * a * b) + (b * b);
+
+	console.log(value);
+
+if (value > 0) {
+
+    console.log('wynik jest dodatni');
+
+} else if (value < 0) {
+
+    console.log('wynik jest ujemny');
+
+} else {
+
+    console.log('wynik = 0');
+
+}
